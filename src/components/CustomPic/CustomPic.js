bf21d6f42093349ddf0666343c87c848674deab8
samarnat <samarnat@mackenzieinvestments.com>
2019-12-18 16:10:01 -0500

    Testing the CustomPic

---
+++ src/components/CustomPic/CustomPic.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import classes from "./CustomPic.module.css";
+
+class CustomPic extends Component {
+  state = {
+    cssprops: [classes.flipboxInner]
+  };
+
+  onFlipHandler = () => {
+    const newCss = [...this.state.cssprops];
+    newCss.push(classes.flip);
+    this.setState({ cssprops: newCss });
+  };
+
+  render() {
+    return (
+      <div
+        className={this.state.cssprops.join(" ")}
+        onClick={this.onFlipHandler}
+      >
+        <div className={classes.flipboxFront}>
+          <p>Hello</p>
+        </div>
+        <div className={classes.flipboxBack}>
+          <p>Im' back</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CustomPic;
--- src/containers/Gameboard/Gameboard.js
+++ src/containers/Gameboard/Gameboard.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Pictures from "../../components/Pictures/Pictures";
-
+import CustomPic from "../../components/CustomPic/CustomPic";
 class Gameboard extends Component {
   render() {
     return (
-      <div>
-        <p>Match the words in the pictures !!</p>
-        <Pictures />
-      </div>
+      //   <div>
+      //     <p>Match the words in the pictures !!</p>
+      //     <Pictures />
+      //   </div>
+      <CustomPic />
     );
   }
 }
