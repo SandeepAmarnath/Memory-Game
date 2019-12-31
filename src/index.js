a3192ff36c03039d5d8abfcc58a8b9e45d2f3669
vijaymalla <malla.vk1992@gmail.com>
2019-12-31 01:49:45 -0500

    Gradiant Backgroung

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Icon from '@material-ui/core/Icon';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
